4ec122e9ce445d48ba39b8f525ea16a67b304050
Matheus Guimaraes <matheusguimr@gmail.com>
2020-09-12 16:00:56 -0300

    Add function getTime()

--- app.js
+++ app.js
@@ -7,55 +7,60 @@ require("dotenv").config();
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const NEWS_URL = "http://www.ufc.br/noticias";
-const CHANNEL = "@noticiasUFC";
-
 const WAIT_TIME = 60;
+const CHANNEL = "@noticiasUFC";
+const NEWS_URL = "http://www.ufc.br/noticias";
 
-const requestLoop = async () => {
+function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+async function getNews(url) {
   try {
-    const response = await crawler.access(NEWS_URL);
-    let news = crawler.scrape(NEWS_URL, response);
+    const response = await crawler.access(url);
+    let news = crawler.scrape(url, response);
     return news;
   } catch (error) {
     return;
   }
-};
+}
 
-function sleep(seconds) {
-  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+function getTime() {
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+
+  return datetime;
 }
 
 async function main() {
   bot.launch();
 
   while (true) {
-    news1 = await requestLoop();
+    let news1 = await getNews(NEWS_URL);
 
-    var currentdate = new Date();
-    var datetime =
-      currentdate.getDate() +
-      "/" +
-      (currentdate.getMonth() + 1) +
-      "/" +
-      currentdate.getFullYear() +
-      " @ " +
-      currentdate.getHours() +
-      ":" +
-      currentdate.getMinutes() +
-      ":" +
-      currentdate.getSeconds();
+    datetime = getTime();
 
     console.log(`[${datetime}]`, news1.headline);
 
     await sleep(WAIT_TIME);
 
-    news2 = await requestLoop();
+    let news2 = await getNews(NEWS_URL);
 
     if (news1.headline !== news2.headline) {
-      console.log(`[${datetime}] nova noticia`);
+      console.log(`[${datetime}] nova noticia:`, news2.headline);
 
-      const message = `${news2.headline}\n${news2.link}`;
+      let message = `${news2.headline}\n${news2.link}`;
       bot.telegram.sendMessage(CHANNEL, message);
     }
   }
@@ -63,6 +68,6 @@ async function main() {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+  console.log(`[INFO] Bot running in port ${port}`);
   main();
-  console.log(`bot running in port ${port}`);
 });
